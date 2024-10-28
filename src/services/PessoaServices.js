888146438b72a390185271cad55c13d9ada1e220
Bruno Tenório <tenoriobn@gmail.com>
2024-10-28 16:11:34 -0300

    Monitorando transações de informações das tabelas

--- src/services/PessoaServices.js
+++ src/services/PessoaServices.js
@@ -1,8 +1,10 @@
+const dataSource = require('../database/models');
 const Services = require('./Services.js');
 
 class PessoaServices extends Services {
   constructor() {
     super('Pessoa');
+    this.matriculaServices = new Services('Matricula');
   }
 
   async pegaMatriculasAtivasPorEstudante(id) {
@@ -21,6 +23,13 @@ class PessoaServices extends Services {
     const listaPessoas = await super.pegaRegistrosPorEscopo('todosOsRegistros');
 
     return listaPessoas;
+  }
+  
+  async cancelaPessoaEMatriculas (estudanteId) {
+    return dataSource.sequelize.transaction(async (transacao) => {
+      await super.atualizaRegistro({ ativo: false }, { id: estudanteId }, transacao);
+      await this.matriculaServices.atualizaRegistro({ status: 'cancelado' }, { estudante_id: estudanteId }, transacao);
+    });
   }
 }
 
